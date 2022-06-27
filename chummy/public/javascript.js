32da19a3e9eafb87cf07ae2e8b6e868c2121d6b9
NaturallyTrash <36828746+NaturallyTrash@users.noreply.github.com>
2022-06-26 20:08:13 -0500

    Add files via upload

---
+++ chummy/public/javascript.js
@@ -0,0 +1,54 @@
+// ./public/javascript.js
+
+// Get the current username from the cookies
+var user = cookie.get('user');
+if (!user) {
+
+  // Ask for the username if there is none set already
+  user = prompt('Choose a username:');
+  if (!user) {
+    alert('{Please enter a username.}');
+  } else {
+    // Store it in the cookies for future use
+    cookie.set('user', user);
+  }
+}
+
+var socket = io();
+
+// The user count. Can change when someone joins/leaves
+socket.on('count', function (data) {
+  $('.user-count').html(data);
+});
+
+// When we receive a message
+// it will be like { user: 'username', message: 'text' }
+socket.on('message', function (data) {
+  (function() {
+    var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+    link.type = 'image/x-icon';
+    link.rel = 'shortcut icon';
+    link.href = 'https://yourdomain.com/favicon.ico';
+    document.getElementsByTagName('head')[0].appendChild(link);
+})();
+  $('.chat').append('<p><strong>' + data.user + '</strong>: ' + data.message + '</p>');
+});
+
+// When the form is submitted
+$('form').submit(function (e) {
+  // Avoid submitting it through HTTP
+  e.preventDefault();
+
+  // Retrieve the message from the user
+  var message = $(e.target).find('input').val();
+
+  // Send the message to the server
+  socket.emit('message', {
+    user: cookie.get('user') || 'Anonymous',
+    message: message
+  });
+
+  // Clear the input and focus it for a new message
+  e.target.reset();
+  $(e.target).find('input').focus();
+});
